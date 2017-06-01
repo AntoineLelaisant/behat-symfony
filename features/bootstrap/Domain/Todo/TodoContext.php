3fd12063541c0309bf119107cf4f2ba70b59a7b2
Antoine Lelaisant <antoine.lelaisant@knplabs.com>
2017-06-01 16:05:53 +0200

    Install behat

---
+++ features/bootstrap/Domain/Todo/TodoContext.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace features\bootstrap\Domain\Todo;
+
+use Behat\Behat\Context\Context;
+
+class TodoContext implements Context
+{
+
+}
---
+++ features/bootstrap/Ui/Todo/TodoContext.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace features\bootstrap\Ui\Todo;
+
+use Behat\Behat\Context\Context;
+use Behat\MinkExtension\Context\RawMinkContext;
+
+class TodoContext extends RawMinkContext implements Context
+{
+
+}
